b664cc253be0d6f23aae2104f5000827ebfc211f
YraKg <yurii.kohan@campus.technion.ac.il>
2024-03-08 00:25:41 +0200

    sobel operator initial implementation

--- filters.py
+++ filters.py
@@ -10,6 +10,7 @@ from numba import cuda
 from numba import njit
 import imageio
 import matplotlib.pyplot as plt
+import numpy as np
 
 def correlation_gpu(kernel, image):
     '''Correlate using gpu
@@ -54,7 +55,24 @@ def sobel_operator():
     pic = load_image()
     # your calculations
 
-    raise NotImplementedError("To be implemented")
+    kernel_0 = np.array([[1,0,-1],[2,0,-2],[1,0,-1]])
+    kernel_1 = np.array([[3,0,-3],[10,0,-10],[3,0,-3]])
+    kernel_2 = np.array([[1,0,-1],[2,0,-2],[1,0,-1],[2,0,-2],[1,0,-1]])
+    kernel_4 = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+
+    sobel_filter = kernel_0
+
+    G_x = correlation_numba(sobel_filter, pic)
+    G_y = correlation_numba(np.transpose(sobel_filter), pic)
+
+    G_x_2 = np.square(G_x)
+    G_y_2 = np.square(G_y)
+    G_sum = G_x_2 + G_y_2
+    G_sqrt = np.sqrt(G_sum)
+
+    return G_sqrt
+
+
 
 
 def load_image(): 
